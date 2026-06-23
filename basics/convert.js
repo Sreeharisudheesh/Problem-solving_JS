57c700d1c7550d6ba52f7c04d345cb30cf083973
sreeharisudheesh1@gmail.com <126215548+Sreeharisudheesh@users.noreply.github.com>
2026-06-23 12:11:18 +0530

    much more done

---
+++ basics/convert.js
@@ -0,0 +1,20 @@
+// 3. Convert a string number "25" into an actual number and add 5 to it.
+
+
+// Before coding, ask:What is the input?
+// What is the output?
+// What transformation is required?
+
+// here,
+
+// Input: "25" (string)
+// Output: 30 (number)
+// Transformation: string → number → add 5
+
+let num="25" 
+
+ let newnum= Number(num)+5;
+
+  console.log( newnum )
+
+  console.log(typeof(newnum))
--- basics/swap.js
+++ basics/swap.js
@@ -1,2 +1,42 @@
 // 2. Swap the values of two variables without using a third variable.
 
+// let a =56 ;
+// let b=65;
+
+// a=a+b; 56+ 65= 121
+// b=a-b; 121- 65= 56
+// a= a-b; 121- 56 = 65
+
+// console.log(`b is  ${b} and a is ${a}`);
+
+// Understand the logic.
+// Derive the code.
+// Don't depend on memory.
+
+// sum - first number = second number
+// sum - second number = first number
+
+// using DESTRUCTURING 
+
+let x= 10;
+let y = 20;
+
+[x,y]=[y,x];
+
+ console.log(x);
+ console.log(y);
+ 
+//  here through destructuring we swap the values with positon that how destructuring works , we use it swap it and prints the value , in this it works postional manner not the values inside 
+
+
+ let a = 5;
+let b = 10;
+let c = 15;
+
+[a, b, c] = [c, a, b];
+15,5,10
+// Left position 1 ← Right position 1
+// Left position 2 ← Right position 2
+// Left position 3 ← Right position 3
+
+// JavaScript evaluates the right side first, then assigns values to the left side.
